a09238ce732bcd66b2b53c1be78eb73636224292
Alyaa AlGhfeli <alya_alshawiii@hotmail.com>
2022-10-11 19:44:23 +0400

    Fix strnstr (casting issue)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: aalghfel <aalghfel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 11:48:20 by aalghfel          #+#    #+#             */
-/*   Updated: 2022/10/10 12:22:13 by aalghfel         ###   ########.fr       */
+/*   Updated: 2022/10/11 19:40:27 by aalghfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (!haystack)
 		return (0);
 	if (!needle)
-		return (haystack);
+		return ((char *)haystack);
 	if (ft_strlen(needle) == 0 || haystack == needle)
 		return ((char *)haystack);
 	while (haystack[i] && i < len)
